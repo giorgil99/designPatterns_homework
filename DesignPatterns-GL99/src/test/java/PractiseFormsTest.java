01f384e1fc905fd86e71d626faa94f41b1fecdef
giorgil99 <giorgilemonjavawork@gmail.com>
2021-09-21 19:14:59 +0400

    small clean up

--- DesignPatterns-GL99/src/test/java/PractiseFormsTest.java
+++ DesignPatterns-GL99/src/test/java/PractiseFormsTest.java
@@ -2,8 +2,6 @@ import com.codeborne.selenide.Condition;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-
 public class PractiseFormsTest {
 
 
